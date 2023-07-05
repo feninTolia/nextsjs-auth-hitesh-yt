9a3036129d09e288b496837aa55ea23ee5ac2398
Anatolii Fenin <fenintolja@gmail.com>
2023-07-05 11:08:57 +0300

    commit 2

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UserProfile = ({ params }: any) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+      <h1>Profile</h1>
+      <hr />
+      <p className="text-4xl">
+        Profile page
+        <span className="p-1 ml-2 rounded bg-green-500 text-black">
+          {params.id}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default UserProfile;
