3231ecbd1220eea62e2054694d8c360e716ceeeb
Anatolii Fenin <fenintolja@gmail.com>
2023-07-13 11:52:48 +0300

    me link

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 
 const ProfilePage = () => {
   const router = useRouter();
+  const [data, setData] = useState('');
 
   const onLogout = async () => {
     try {
@@ -19,12 +21,22 @@ const ProfilePage = () => {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me');
+    console.log('res', res.data);
+    setData(res.data.data._id);
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       <h1>Profile</h1>
       <hr />
       <h2>ProfilePage</h2>
       <hr />
+      {data ? <Link href={`/profile/${data}`}>Go to your profile</Link> : null}
       <button
         className="bg-blue-500 hover:bg-blue-700 border p-2 rounded-lg mb-4 mt-4"
         onClick={onLogout}
